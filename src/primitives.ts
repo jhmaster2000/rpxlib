1c4310213b9ea3ff1eefd72e399626912d704047
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-08-01 04:22:48 -0300

    add zlib compression levels type

--- src/primitives.ts
+++ src/primitives.ts
@@ -61,3 +61,5 @@ export class sint32 extends Number {
 //type sint16 = number;
 //type sint32 = number;
 export type int = uint8 | uint16 | uint32 | sint8 | sint16 | sint32;
+
+export type ZlibCompressionLevels = -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
