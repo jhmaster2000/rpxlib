6061d3a5afe806716144d118b2c74ba1911e44e9
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-08-04 03:24:41 -0300

    primitives allow ints as input

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,55 +1,55 @@
 export type nybble = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
 
 export class uint8 extends Number {
-    constructor(x: number | string = 0) {
+    constructor(x: number | string | int = 0) {
         x = <number>x >>> 0;
-        if (x < 0) throw new RangeError(`u8 underflow: ${x}`);
-        if (x > 255) throw new RangeError(`u8 overflow: ${x}`);
+        if (x < 0) throw new RangeError(`u8 underflow: ${+x}`);
+        if (x > 255) throw new RangeError(`u8 overflow: ${+x}`);
         super(x);
     }
 }
 
 export class uint16 extends Number {
-    constructor(x: number | string = 0) {
+    constructor(x: number | string | int = 0) {
         x = <number>x >>> 0;
-        if (x < 0) throw new RangeError(`u16 underflow: ${x}`);
-        if (x > 65535) throw new RangeError(`u16 overflow: ${x}`);
+        if (x < 0) throw new RangeError(`u16 underflow: ${+x}`);
+        if (x > 65535) throw new RangeError(`u16 overflow: ${+x}`);
         super(x);
     }
 }
 
 export class uint32 extends Number {
-    constructor(x: number | string = 0) {
+    constructor(x: number | string | int = 0) {
         x = <number>x >>> 0;
-        if (x < 0) throw new RangeError(`u32 underflow: ${x}`);
-        if (x > 4294967295) throw new RangeError(`u32 overflow: ${x}`);
+        if (x < 0) throw new RangeError(`u32 underflow: ${+x}`);
+        if (x > 4294967295) throw new RangeError(`u32 overflow: ${+x}`);
         super(x);
     }
 }
 
 export class sint8 extends Number {
-    constructor(x: number | string = 0) {
+    constructor(x: number | string | int = 0) {
         x = <number>x >> 0;
-        if (x < -128) throw new RangeError(`s8 underflow: ${x}`);
-        if (x > 127) throw new RangeError(`s8 overflow: ${x}`);
+        if (x < -128) throw new RangeError(`s8 underflow: ${+x}`);
+        if (x > 127) throw new RangeError(`s8 overflow: ${+x}`);
         super(x);
     }
 }
 
 export class sint16 extends Number {
-    constructor(x: number | string = 0) {
+    constructor(x: number | string | int = 0) {
         x = <number>x >> 0;
-        if (x < -32768) throw new RangeError(`s16 underflow: ${x}`);
-        if (x > 32767) throw new RangeError(`s16 overflow: ${x}`);
+        if (x < -32768) throw new RangeError(`s16 underflow: ${+x}`);
+        if (x > 32767) throw new RangeError(`s16 overflow: ${+x}`);
         super(x);
     }
 }
 
 export class sint32 extends Number {
-    constructor(x: number | string = 0) {
+    constructor(x: number | string | int = 0) {
         x = <number>x >> 0;
-        if (x < -2147483648) throw new RangeError(`s32 underflow: ${x}`);
-        if (x > 2147483647) throw new RangeError(`s32 overflow: ${x}`);
+        if (x < -2147483648) throw new RangeError(`s32 underflow: ${+x}`);
+        if (x > 2147483647) throw new RangeError(`s32 overflow: ${+x}`);
         super(x);
     }
 }
