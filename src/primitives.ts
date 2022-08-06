41925d92a189f0aef7237e385b38c063baccaf59
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-08-06 01:24:47 -0300

    BaseAddr constants

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,3 +1,7 @@
+export const CodeBaseAddress = 0x02000000 as const;
+export const DataBaseAddress = 0x10000000 as const;
+export const LoadBaseAddress = 0xC0000000 as const;
+
 export type nybble = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15;
 
 export class uint8 extends Number {
