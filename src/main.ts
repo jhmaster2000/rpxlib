7274de87c62dbd415d3cec4b328416780a1ea0e7
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-08-06 01:26:08 -0300

    updt. main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { debug } from './debug'; debug;
 import { RPL } from './rpl';
 import { WSLSafePath } from './wslsafepath';
+import { SectionFlags, SectionType } from './enums';
+import { CodeBaseAddress, DataBaseAddress } from './primitives';
+import { Section } from './sections';
 import fs from 'fs';
 
 console.time('total');
@@ -12,13 +15,30 @@ const rpx = new RPL(RPX_DATA);
 console.timeEnd('parse file');
 
 //console.time('debug file');
-//await debug(rpx, { rplfileinfo: true });
+//await debug(rpx);
 //console.timeEnd('debug file');
 
 const rplcrcsStrAddr = rpx.shstrSection.strings.add('.rplcrcs');
 const rplfileinfoStrAddr = rpx.shstrSection.strings.add('.rplfileinfo');
 rpx.crcSection.nameOffset = rplcrcsStrAddr;
 rpx.fileinfoSection.nameOffset = rplfileinfoStrAddr;
+
+const nextFreeTextAddr = rpx.addressRanges.free.find(([startAddr]) => startAddr >= CodeBaseAddress && startAddr < DataBaseAddress)![0];
+
+const syscall = new Section({
+    nameOffset: rpx.shstrSection.strings.add('.haxx.syscall'),
+    type: SectionType.ProgBits,
+    flags: SectionFlags.Executable | SectionFlags.Alloc,
+    addr: nextFreeTextAddr,
+    link: 0,
+    info: 0,
+    addrAlign: 0x20,
+    entSize: 0,
+    data: Buffer.from('60000000600000004E800020', 'hex')
+}, rpx);
+rpx.pushSection(syscall);
+
+rpx.fileinfoSection.adjustFileInfoSizes();
 
 console.time('save file');
 const savedTo = rpx.save('output3');
